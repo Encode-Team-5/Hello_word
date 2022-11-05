5c4730b563be9f4e9619c104aa44b45bdc5b7039
"smart_contract" <"chijioke.emma.maxwell@gmail.com">
2022-11-05 17:16:41 +0100

    update smart contract

---
+++ contracts/helloworld.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
+
+contract HelloWorld {
+    string private text;
+    address public owner;
+
+    constructor(string memory) {
+        text ="Hello world";
+        owner = msg.sender;
+    }
+
+    function helloWorld() public view returns (string memory) {
+        return text;
+    }
+
+    function setText(string calldata newText) public onlyOwner {
+        text = newText;
+    }
+
+    function transferOwnership(address newOwner) public onlyOwner {
+        owner = newOwner;
+    }
+
+    modifier onlyOwner()
+    {
+        require (msg.sender == owner, "Only Owner Can Call This Contract");
+        _;
+    }
+}
